22adcb0c0b2e115e67f633e8cd908fdefd013173
“Sage <sage.mns@gmail.com>
2026-03-17 15:47:17 -0400

    Updated sore.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+
+/**
+ * Task 5: Configuring the Redux Store
+ * This store manages the global state of the application.
+ * The 'cart' slice of the state is managed by the cartReducer from CartSlice.jsx.
+ */
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
+
+// Export the store to be used by the <Provider> in main.jsx or App.jsx
+export default store;
+
+/* import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
+export default store */
