f2e1b598cad233aee38fc8566598364244faf8f8
“Sage <sage.mns@gmail.com>
2026-03-17 15:49:49 -0400

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
+/**
+ * Task 6: Global Store Setup
+ * Wrapping the App component with the Redux Provider makes the 
+ * global state accessible to all nested components.
+ */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
+
+/* import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -10,4 +30,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     </Provider>
   </React.StrictMode>,
-)
+) */
